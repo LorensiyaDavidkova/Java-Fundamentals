4048b55778424dd83b8d0f605c84b862f75cab29
LorensiyaDavidkova <93177416+LorensiyaDavidkova@users.noreply.github.com>
2022-09-22 10:48:19 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         //I am here in this file source!
         System.out.println("Hello world!");
+        //I made correction from the github
     }
 }
